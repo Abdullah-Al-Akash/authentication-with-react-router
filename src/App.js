a2f8d6628fbb2e596eb6a28180d50ec74cd9cfe7
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-04-16 02:51:57 +0600

    Initialize Authentication with React Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { Form, Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import app from "./firebase.init";
+import { useState } from "react";
 
 function App() {
+  const auth = getAuth(app);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [registered, setRegistered] = useState(false);
+
+  const handleEmailBlur = e => {
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordBlur = e => {
+    setPassword(e.target.value);
+  }
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    if (registered) {
+      signInWithEmailAndPassword(auth, email, password)
+        .then(result => {
+          const user = result.user;
+          console.log(user);
+        })
+        .catch(error => {
+          setError(error.message);
+          console.log(error);
+        })
+    }
+
+    else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(result => {
+          const user = result.user;
+          console.log(user);
+          setPassword('');
+          setEmail('');
+          setError('');
+        })
+        .catch(error => {
+          console.log(error);
+          setError(error.message);
+        })
+    }
+  }
+
+  // Handle Registered Check:
+  const handleRegistered = e => {
+    setRegistered(e.target.checked);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className="w-50 mx-auto">
+        <h2 className="m-4">Please {registered ? 'Login' : 'Register'}!!!</h2>
+        <Form onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="EnterPassword" />
+          </Form.Group>
+          <p className="text-danger">{error}</p>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check onChange={handleRegistered} type="checkbox" label="Already Registered?" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            {registered ? 'Login' : 'Register'}
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+        apiKey: "AIzaSyArHLmsKDBPvbg5hvlOuNAACnykUw-x0kE",
+        authDomain: "simple-firebase-authenti-c6bf2.firebaseapp.com",
+        projectId: "simple-firebase-authenti-c6bf2",
+        storageBucket: "simple-firebase-authenti-c6bf2.appspot.com",
+        messagingSenderId: "178145318825",
+        appId: "1:178145318825:web:5fd9015d934c2192d856b8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
